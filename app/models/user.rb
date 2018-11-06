f1336d5478526e27a53fdec02f7bc07201e25cc0
s.sato <samurai310@aol.jp>
2018-11-06 10:41:10 +0000

    画像アップロード課題機能を搭載

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :name, presence: true,length: { maximum: 30 }
   validates :email, presence: true,length: { maximum: 255 },format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :email, uniqueness: true
+  validates :image, presence: true
   
   before_validation { email.downcase! }
   has_secure_password
@@ -10,4 +10,5 @@ class User < ApplicationRecord
   has_many :blogs
   has_many :favorites, dependent: :destroy
   has_many :favorite_blogs, through: :favorites, source: :blog, dependent: :destroy
+  mount_uploader :image, ImageUploader
 end
